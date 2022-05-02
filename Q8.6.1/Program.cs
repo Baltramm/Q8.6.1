4fdc97524d26daba0241f054d26bd15c35a70d4b
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-05-02 21:59:09 +0600

    Добавьте файлы проекта.

---
+++ Q8.6.1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+namespace Q8._6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            string filePath = @"C://Filex";
+            if (!File.Exists(filePath))
+            {
+                try
+                {
+                    var directory = new DirectoryInfo(filePath);
+                    foreach (FileInfo file in directory.GetFiles())
+                    {
+
+                        file.Delete();
+
+
+                    }
+                    foreach (DirectoryInfo direc in directory.GetDirectories())
+                    {
+
+                        direc.Delete(true);
+
+
+                    }
+
+
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+    }
+}
